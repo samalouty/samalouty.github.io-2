71ce5e409dbd68f5a1c8c6d25ced0a0d6a8787e1
samalouty <128055267+samalouty@users.noreply.github.com>
2023-07-08 16:22:33 +0300

    clear button functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const mazeTable = document.getElementById('maze');
   const mazeBody = mazeTable.querySelector('tbody');
   
@@ -12,5 +12,17 @@ window.addEventListener('DOMContentLoaded', () => {
       newRow.appendChild(newCell);
     }
     mazeBody.appendChild(newRow);
+  }
+  const targetCell = mazeTable.rows[2].cells[2];
+  targetCell.style.backgroundColor = '#ff79c6'; //colouring one cell
+
+  const clearBtn = document.querySelector('.clear-btn');
+  clearBtn.addEventListener('click', clearBoard);
+
+  function clearBoard() {
+    const cells = mazeTable.querySelectorAll('td');
+    cells.forEach((cell) => {
+      cell.style.backgroundColor = 'black';
+    });
   }
 });
